689e24987b7aa49a2e363c6d246a3a151ba18249
Felix Eckardt <felix.eckardt@online.de>
2021-03-10 19:48:12 +0100

    Refactor client

--- client/client.cpp
+++ client/client.cpp
@@ -16,9 +16,17 @@ constexpr auto USE_POISSON = false;
 constexpr auto POISSON_LAMBDA = 3.5;
 constexpr auto SETUP_TIME = 10;
 
+struct ThreadData {
+  std::atomic<bool> keep_running{true};
+  std::atomic<bool> count_events{false};
+  std::atomic<uint64_t> event_count{0};
+  char* server_addr;
+  uint16_t port;
+};
+
 void msgHandler(int fd, std::vector<uint8_t>& data)
 {
-  //std::cout << "recv(" << fd << "): " << std::string(data.begin(), data.end()) << "\n";
+  // std::cout << "recv(" << fd << "): " << std::string(data.begin(), data.end()) << "\n";
   // send random number
   std::string s = std::to_string(rand());
   auto msg = Net::wrapMessage(reinterpret_cast<const uint8_t*>(s.c_str()), s.length());
@@ -34,18 +42,14 @@ void msgHandler(int fd, std::vector<uint8_t>& data)
   }
 }
 
-void runThread(const std::atomic<bool>& keep_running,
-               const std::atomic<bool>& count_events,
-               std::atomic<uint64_t>& ev_count,
-               const char* server_addr,
-               const uint16_t port)
+void runThread(ThreadData& thread_data)
 {
   // init socket
   struct sockaddr_in address;
   memset(&address, 0, sizeof(address));
   address.sin_family = AF_INET;
-  inet_aton(server_addr, &(address.sin_addr));
-  address.sin_port = htons(port);
+  inet_aton(thread_data.server_addr, &(address.sin_addr));
+  address.sin_port = htons(thread_data.port);
   // connect to server
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (connect(sockfd, (struct sockaddr*)&address, sizeof(address)) == -1) {
@@ -57,12 +61,14 @@ void runThread(const std::atomic<bool>& keep_running,
   uint64_t local_ev_count = 0;
   std::default_random_engine generator;
   std::exponential_distribution<double> distribution(POISSON_LAMBDA);
-  Net::PacketProtocol packetFrame([&](auto& data) {
-    if (count_events)
+
+  Net::PacketProtocol packetizer([&](auto& data) {
+    // count events
+    if (thread_data.count_events)
       local_ev_count++;
 
+    // wait for an exponential distributed amount of time (poisson process)
     if (USE_POISSON) {
-      // wait for an exponential distributed amount of time (poisson process)
       int i = 1000 * distribution(generator);
       std::this_thread::sleep_for(std::chrono::milliseconds(i));
     }
@@ -78,18 +84,19 @@ void runThread(const std::atomic<bool>& keep_running,
     exit(EXIT_FAILURE);
   }
 
-  while (keep_running) {
+  // main loop
+  while (thread_data.keep_running) {
     // wait for data and read in
     int n = read(sockfd, buf, sizeof(buf));
     if (n < 0) {
       perror("read()");
       exit(EXIT_FAILURE);
     }
-    packetFrame.receive(buf, n);
+    packetizer.receive(buf, n);
   }
 
   close(sockfd);
-  ev_count += local_ev_count;
+  thread_data.event_count += local_ev_count;
 }
 
 int main(int argc, char* argv[])
@@ -99,11 +106,12 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  ThreadData thread_data;
+  thread_data.server_addr = argv[1];
+  thread_data.port = std::stoi(argv[2]);
+
   uint thread_count;
-  uint dead_conns;
   uint run_seconds;
-  char* server_addr = argv[1];
-  uint16_t port = std::stoi(argv[2]);
 
   try {
     thread_count = std::stoi(argv[3]);
@@ -115,28 +123,25 @@ int main(int argc, char* argv[])
   }
 
   // start threads
-  std::atomic<bool> keep_running{true};
-  std::atomic<bool> count_events{false};
-  std::atomic<uint64_t> event_count{0};
   std::vector<std::thread> threads;
   for (int t_i = 0; t_i < thread_count; t_i++) {
-    threads.emplace_back([&]() { runThread(keep_running, count_events, event_count, server_addr, port); });
+    threads.emplace_back(runThread, std::ref(thread_data));
   }
 
   sleep(SETUP_TIME);
   // start counting events
-  count_events = true;
+  thread_data.count_events = true;
   sleep(run_seconds);
 
   // stop threads
-  keep_running = false;
+  thread_data.keep_running = false;
   for (auto& thread : threads) {
     thread.join();
   }
 
-  std::cout << event_count << " events in " << run_seconds << "s\n"
-            << (event_count * 1.0) / run_seconds << " events per second\n"
-            << (run_seconds * 1000.0) / event_count << " ms per event" << std::endl;
+  std::cout << thread_data.event_count << " events in " << run_seconds << "s\n"
+            << (thread_data.event_count * 1.0) / run_seconds << " events per second\n"
+            << (run_seconds * 1000.0) / thread_data.event_count << " ms per event" << std::endl;
 
   return 0;
 }
